d8f5c7134db2c04cb25d4fc4662ffae8712618e0
Biloliddin <biloliddin500dbmin@gmail.com>
2022-05-04 12:55:51 +0500

    some fixes

--- projects/food/js/script.js
+++ projects/food/js/script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     tabs('.tabheader__item', '.tabcontent', 
          '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2021-05-20');
+    timer('.timer', '2022-11-22');
     cards();
     calc();
     forms('form', modalTimerId);
